f9950eb000d83e4e8d267576d394c37d083fb1f3
Mary-Maina <richellwanjiru@gmail.com>
2024-08-03 14:36:33 -0700

    110-binary_tree_is_bst.c

--- binary_trees.h
+++ binary_trees.h
@@ -61,4 +61,5 @@ void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
